3cf02822e4fa84bbad43f712c5b994b602e9032c
daraildes <dara.aragao17@gmail.com>
2024-03-12 19:10:00 -0300

    Inclusao do arquivo de email e templete (preciso corrigir ainda)

--- interpolacao.py
+++ interpolacao.py
@@ -1,28 +1,35 @@
-email_tmpl = """
-
-Ola %(nome)s,
-
-Tem interesse em comprar %(produto)s?
-
-Este produto é ótimo para resolver %(texto)s.
-
-Clique agora em %(link)s.
-
-Apenas %(quantidade)d disponíveis!
-
-Preço promocional %(preco).2f.
 """
+__Version_ = 0.1.1"""
 
-clientes = ["Rita", "Lourdes", "Paloma"]
+import sys
+import os
 
-for cliente in clientes:
-    print(
-        email_tmpl % {
-            "nome": cliente,
+arguments = sys.argv[1:]
+if not arguments: 
+   print("informa o nome do arquivo de emails")
+   sys.exit(1)
+   
+filename = arguments [0]
+templatename = arguments [1]
+
+path = os.curdir
+filepath = os.path.join(path, filename) # emails.txt
+templatepath = os.path.join(path, templatename) # email_tmpl.txt
+
+for line in open(filepath):
+     name, email = line.split(",")
+
+  # TODO: Substituir por envio de e-mail
+  print(f"Enviando email para: {email}")
+  print()
+  print(
+        open(templatepath).read() % {
+            "nome": name,
             "produto": "caneta",
             "texto": "Escrever muito bem.",
             "link": "https://canetaslegais.com",
             "quantidade": 1,
-            "preco": 20.6
+            "preco": 20.6           
         }
     )
+    print("-" * 50)
